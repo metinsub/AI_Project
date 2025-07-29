713b174774c0ff4ba2acd70bc947b54706507cb3
metinsub <metin-subasi@outlook.de>
2025-07-30 00:09:18 +0200

    Started Vector db

--- Agentic_RAG/api/services/vector_service.py
+++ Agentic_RAG/api/services/vector_service.py
@@ -1,1 +1,12 @@
-# Vector Database Service 
+# Vector Database Service
+from sentence_transformers import SentenceTransformer
+
+class VectorService:
+    def __init__(self):
+        
+
+    def create_index(self):
+        
+    def store_chunks(self, chunks)
+
+    def search_similar(self, query)
